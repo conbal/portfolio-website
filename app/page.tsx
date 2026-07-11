dea415d52d13ea01c4d6f63b88b4754196d92316
conbal <connorbaldwin2001@gmail.com>
2026-07-11 17:56:25 -0500

    updated file structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
 "use client";
 
+import Nav from "@/components/Nav";
+import About from "@/components/sections/About";
+import Contact from "@/components/sections/Contact";
+import Projects from "@/components/sections/Projects";
+import Skills from "@/components/sections/Skills";
+
 export default function Page() {
-  function handleClick() {
-    alert("button pressed");
-  }
   return (
-    <div className="flex space-x-3 justify-center items-center h-screen">
-      <div>app page </div>
-      <button
-        type="button"
-        onClick={handleClick}
-        className="bg-blue-800 hover:bg-blue-500 text-white font-medium px-4 py-2 rounded-lg transition cursor-pointer "
-      >
-        button here
-      </button>
+    <div
+      className="flex space-x-3 justify-center items-center h-screen"
+      suppressHydrationWarning
+    >
+      <Nav />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
     </div>
   );
 }
